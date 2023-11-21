be77fcb8d107ec7a791846605e51a7ddea951c81
OrangeWolfy <orange007wolfy@gmail.com>
2023-11-21 00:03:01 -0600

    Calculator in C

---
+++ Calculator.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main(){
+    int n1, n2, b, res;
+    char op;
+
+    printf("Calculator\n");
+    printf("Enter your operation: ");
+    scanf("%d %c %d", &n1, &op, &n2);
+    switch(op){
+        case '+':
+            res = n1 + n2;
+            break;
+        case '-':
+            res = n1 - n2;
+            break;
+        case '*':
+        case 'x':
+            res = n1 * n2;
+            break;
+        case '/':
+            res = n1 / n2;
+            break;
+        default:
+            printf("Invalid operation\n");
+            return 1;
+            break;
+    }
+    printf("%d %c %d = %d\n", n1, op, n2, res);
+    return 0;
+}
+
